b336fc0fc60e186df83977f7b397c55efd3bff7b
RRuslanIv <r.rusachenka@gmail.com>
2019-03-19 11:24:06 +0300

    check valid sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   var config = bracketsConfig.reduce(function(prev, curr) {
+    prev[curr[0]] = curr[1];
+    return prev;
+  }, {})
+	
+  var arr = [], arrClose = [], current
+  for (var i = 0; i < str.length; i++) {
+    current = str[i]
+    if (config[current] && !arr.includes(current)) {
+      arr.push(current)
+    } else {
+      if (current !== config[arr.pop()] && arr.includes(current)) {
+        arrClose.push(current)
+      }
+    } 
+  }
+  return arr.length == arrClose.length ? true : false
 }
